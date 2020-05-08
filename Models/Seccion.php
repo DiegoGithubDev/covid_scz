47ef5ae7c9aad0184187334e85b3f276d0b5c085
diego <diego@serverfedo>
2020-05-08 02:19:22 -0400

    create seccin model

--- Models/Seccion.php
+++ Models/Seccion.php
@@ -1,8 +1,50 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<?php namespace Models;
+class Seccion {
+    private $id;
+    private $nombre;
+    private $con ;
+    
+    public function __construct() {
+        $this->$con = new Conexion();
+    }
+    
+    public function set($atributo,$contenido) {
+        $this->$atributo = $contenido;
+        
+    }
+    
+    public function get($atributo) {
+        return $this->$atributo ;
+    }
+    
+    public function listar() {
+        $sql = "SELECT * FROM seccion";
+        $datos = $this->con->consultaRetorno($sql);
+        return $datos;
+        
+    }
+    
+    public function add() {
+        $sql = "INSERT INTO seccion(id,nombre) VALUES(null,'{$this->nombre}')";
+        $this->con->consultaSimple($sql);
+    }
+    
+    public function delete() {
+        $sql = "DELETE FROM seccion WHERE id='{$this->id}'";
+        $this->con->consultaSimple($sql);
+    }
+    
+    public function edit($param) {
+        $sql = "UPDATE FROM seccion SET nombre = '{$this->nombre}' WHERE id ='{$this->id}'";
+        $this->con->consultaSimple($sql);
+    }
+    
+    public function view($param) {
+        $sql = "SELECT * FROM seccion WHERE id = '{$this->id}'";
+        $datos = $this->con->consultaRetorno($sql);
+        $row = mysql_fetch_array($datos);
+        return $row;
+    }
+    
+    
+}
