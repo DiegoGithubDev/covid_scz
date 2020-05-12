ae0fed73ce9c6d5febc5a2da42d4e76e86ef32e5
diego <diego@serverfedo>
2020-05-12 18:02:26 -0400

    add values default to request

--- Config/Request.php
+++ Config/Request.php
@@ -7,15 +7,24 @@ class Request{
     public function __construct() {
         if ( isset($_GET['url']) ){
             $ruta = filter_input(INPUT_GET, 'url',FILTER_SANITIZE_URL);
+            //echo "esto es la ruta antes explode= $ruta <br> ";
             $ruta = explode('/', $ruta);
             $ruta = array_filter($ruta);
+           // echo "esto es la ruta = $ruta <br> ";
             $this->controlador = strtolower(array_shift($ruta));
             $this->metodo = strtolower(array_shift($ruta));
+            $this->argumento = strtolower(array_shift($ruta));
+            if (!$this->metodo){
+                $this->metodo = 'index'; 
+            }
+        }else{
+             if (!$this->controlador){
+                $this->controlador = 'estudiantes'; 
+            }
             if (!$this->metodo){
                 $this->metodo = 'index';
-            }
-            $this->argumento = strtolower(array_shift($ruta));
-        }
+            }  
+        }        
     }
     
     public function getControlador() {
