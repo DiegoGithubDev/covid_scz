705f01c6627681f3daf113a56b57b1d9d97ddce7
diego <diego@serverfedo>
2020-05-22 06:26:58 -0400

    view student in a table

--- Views/estudiantes/index.php
+++ Views/estudiantes/index.php
@@ -1,6 +1,28 @@
-<h1>vista de estudiante</h1>
-<?php  $datos=$estudiantes->index();
-  while ($row = mysqli_fetch_array($datos)) {
-      echo $row["nombre"];
-    }
-?>
+
+    <div class="container" > 
+        <div class="">
+            <h1 class="display-4">Lista de Estuiantes</h1>
+            <table class="table table-hover">
+              <thead>
+                <tr class="bg-primary" >
+                  <th scope="col">Imagen</th>
+                  <th scope="col">Nombre</th>
+                  <th scope="col">Edad</th>
+                  <th scope="col">Promedio</th>
+                </tr>
+              </thead>
+              <tbody>
+                  <?php $datos=$estudiantes->index();
+                    while ($row = mysqli_fetch_array($datos)) { ?>
+                    <tr>
+                        <td><?php echo $row["imagen"]; ?></td>
+                        <td><?php echo $row["nombre"]; ?></td>
+                        <td><?php echo $row["edad"];   ?></td>
+                        <td><?php echo $row["promedio"]; ?></td>
+                    </tr>
+                  <?php } ?>
+              </tbody>
+            </table> 
+        </div>
+    </div> 
+    
