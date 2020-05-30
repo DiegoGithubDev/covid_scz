b6b1669143bc9156d3a60d62fabf61f1cca23826
diego <diego@serverfedo>
2020-05-29 23:41:28 -0400

    create controller and its actiosn of persona

---
+++ Controllers/personasController.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Controllers;
+
+/**
+ * Description of personasController
+ *
+ * @author diego
+ */
+class personasController {
+    private $persona ;
+    
+    public function index(){
+        
+    }
+    public function agregar() {
+        
+    }
+    public function edidtar() {
+        
+    }
+    public function eliminar(){
+        
+    }
+}
