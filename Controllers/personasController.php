7ce5bd04bfa1e86e3e8a1490bd032ad3b0584959
diego <diego@serverfedo>
2020-05-30 04:23:50 -0400

    sent datas from view to controller and to model

--- Controllers/personasController.php
+++ Controllers/personasController.php
@@ -11,22 +11,26 @@ class personasController {
         return $datos;
     }
     public function agregar() {
-        $nombre = $_POST['input_nombre'] ;
-        $apellido_paterno = $_POST['input_apellido_paterno'];
-        $apellido_materno= $_POST['input_estado_materno'];
-        $estado_salud = $_POST['input_estado_salud'];
-        $telefono = $_POST['input_telefono'] ;
-        $ubucacion= $_POST['input_ubicacion'];
-        
-        $this->persona->set("nombre", $nombre);
-        $this->persona->set("apellido_paterno", $apellido_paterno);
-        $this->persona->set("apellido_materno", $apellido_materno);
-        $this->persona->set("estado_salud", $estado_salud);
-        $this->persona->set("telefono", $telefono);
-        $this->persona->set("ubicacion", $ubucacion);
-        
-        $this->persona->add();
+        if ($_REQUEST['POST'] === 'POST') {
+            $nombre = $_POST['input_nombre'];
+            $apellido_paterno = $_POST['input_apellido_paterno'];
+            $apellido_materno = $_POST['input_estado_materno'];
+            $estado_salud = $_POST['input_estado_salud'];
+            $telefono = $_POST['input_telefono'];
+            $ubucacion = $_POST['input_ubicacion'];
+
+            $this->persona->set("nombre", $nombre);
+            $this->persona->set("apellido_paterno", $apellido_paterno);
+            $this->persona->set("apellido_materno", $apellido_materno);
+            $this->persona->set("estado_salud", $estado_salud);
+            $this->persona->set("telefono", $telefono);
+            $this->persona->set("ubicacion", $ubucacion);
+            $this->persona->add();
+            $url_personas = "Location:" . URL . "personas/index";
+            header($url_personas);
+        }
     }
+
     public function edidtar() {
         
     }
