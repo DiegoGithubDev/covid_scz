96b4821763b49151432deecb5300a08b3abfa886
diego <diego@serverfedo>
2020-05-31 04:23:48 -0400

    take datas from person for edit

--- Controllers/personasController.php
+++ Controllers/personasController.php
@@ -32,8 +32,8 @@ class personasController {
     }
 
     public function editar() {
+        $id = $_POST['input_id'];
         if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-            $id = $_POST['input_id'];
             $nombre = $_POST['input_nombre'];
             $apellido_paterno = $_POST['input_apellido_paterno'];
             $apellido_materno = $_POST['input_apellido_materno'];
@@ -50,6 +50,10 @@ class personasController {
             $this->persona->edit();
             $list_person = "Location: ".URL."/persona/index";
             header($list_person);
+        }else{
+            $this->persona->set("id", $id);
+            $datos = $this->persona->view();
+            return $datos;
         }
     }
     public function eliminar(){
