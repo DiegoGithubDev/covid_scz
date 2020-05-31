f1dc7abbaa56cc849c508eb849ddeab6e0426278
diego <diego@serverfedo>
2020-05-31 04:51:19 -0400

    show up person data

--- Views/personas/editar.php
+++ Views/personas/editar.php
@@ -7,34 +7,34 @@
                    <fieldset>
                     <div class="form-group">
                         <label class="col-form-label" for="input_id">id</label>
-                        <input name="input_id" disabled="" class="form-control" id="input_id" type="text">
+                        <input name="input_id" disabled="" value="<?php echo $datos['id']?>"class="form-control" id="input_id" type="text">
                     </div>
                     <div class="form-group">
                         <label class="col-form-label" for="input_nombre">Nombre</label>
-                        <input name="input_nombre" class="form-control" id="id_input_nombre" type="text">
+                        <input name="input_nombre" value="<?php echo $datos['nombre']?>" class="form-control" id="id_input_nombre" type="text">
                     </div>
                     <div class="form-group">
                         <label class="col-form-label" for="input_apellido_paterno">Apellido Paterno</label>
-                        <input name="input_apellido_paterno" class="form-control" id="input_apellido_paterno" type="text">
+                        <input name="input_apellido_paterno" value="<?php echo $datos['apellido_paterno']?>" class="form-control" id="input_apellido_paterno" type="text">
                     </div>
                     <div class="form-group">
-                        <label class="col-form-label" for="input_apellido_materno">Apellido Paterno</label>
-                        <input name="input_apellido_materno" class="form-control" id="input_apellido_materno" type="text">
+                        <label class="col-form-label" for="input_apellido_materno">Apellido Materno</label>
+                        <input name="input_apellido_materno" value="<?php echo $datos['apellido_materno']?>" class="form-control" id="input_apellido_materno" type="text">
                     </div>
                     <div class="form-group">
                         <label for="exampleSelect1">Estado de salud</label>
                         <select name = "input_estado_salud" class="form-control" id="exampleSelect1">
-                            <option value="sano" >sano</option>
-                            <option value="enfermo" >enfermo de covid</option>
+                            <option value="<?php $datos['estado_salud']?>" ><?php echo $datos['estado_salud']?></option>
+                            <option value="<?php echo ($datos['estado_salud']=='sano')?'enfermo':'sano';?>" ><?php echo ($datos['estado_salud']=='sano')?'enfermo':'sano';?></option>
                         </select>
                     </div>
                     <div class="form-group">
                         <label class="col-form-label" for="input_telefono">Telefono</label>
-                        <input name="input_telefono" class="form-control" id="input_telefono" type="text">
+                        <input name="input_telefono" value="<?php echo $datos['telefono']?>"  class="form-control" id="input_telefono" type="text">
                     </div>
                     <div class="form-group">
                         <label class="col-form-label" for="input_ubicacion">Ubicacion</label>
-                        <input name="input_ubicacion" class="form-control" id="input_ubicacion" type="text">
+                        <input name="input_ubicacion"  value="<?php echo $datos['ubicacion']?>" class="form-control" id="input_ubicacion" type="text">
                     </div>
                     <button type="submit" class="btn btn-primary">Editar</button>
                 </fieldset>
