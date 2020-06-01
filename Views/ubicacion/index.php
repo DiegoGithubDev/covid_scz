70e572860a2926ddb0417a84bc4714b6c1360001
diego <diego@serverfedo>
2020-06-01 06:31:00 -0400

    hide input

--- Views/ubicacion/index.php
+++ Views/ubicacion/index.php
@@ -4,25 +4,23 @@
 <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 <script src="<?php echo URL ?>Views/ubicacion/grafica_datos.js" type="text/javascript"></script>
 <link href="<?php echo URL ?>Views/ubicacion/mapa.css" rel="stylesheet" type="text/css"/>
-
-    <h3 id="info">My Google Maps Demo</h3>
-    <!--The div element for the map -->
-    <div class="container" > 
-        <div class="">
-             <div id="map"></div>
-        </div>
-        <div class="">
-            <button id='btn_infectados' onclick="get_features()" type="button" class="btn btn-danger">listar infectados</button>
-            <?php while ($row = mysqli_fetch_array($datos)) {
-                    $json[]=array(
-                                'latitud'=>$row['latitud'],
-                                'longitud'=>$row['longitud']
-                            ); 
-            }?>
-            <input id="input_cordenadas_json"value='<?php echo json_encode($json); ?>'>
-        </div>
-        <div id="chartContainer" style="height: 300px; width: 100%;"></div>
-    </div> 
+<div class="container" > 
+    <div class="">
+        <div id="map"></div>
+    </div>
+    <div class="">
+        <button id='btn_infectados' onclick="get_features()" type="button" class="btn btn-danger">listar infectados</button>
+        <?php
+        while ($row = mysqli_fetch_array($datos)) {
+            $json[] = array(
+                'latitud' => $row['latitud'],
+                'longitud' => $row['longitud']
+            );
+        }
+        ?>
+        <input style="display: none" id="input_cordenadas_json"value='<?php echo json_encode($json); ?>'>
+    </div>
+    <div id="chartContainer" style="height: 300px; width: 100%;"></div>
+</div> 
 <script src="<?php echo URL ?>Views/ubicacion/ubicacion.js" type="text/javascript"></script>
 
-    
